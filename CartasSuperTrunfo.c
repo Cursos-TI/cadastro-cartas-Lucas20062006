37530bc8f9f6d8c707292af5a25553c6298ee9a1
Lucas20062006 <lucasedudu11@gmail.com>
2025-09-26 14:56:45 +0000

    segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 
-int main("carta 01!/n"){
- // Estado
-  char São Paulo
+int main() {
+  printf("carta 01!/n");
+ //Estado
+  char Estado = "São_Paulo";
 //Codigo
-  char[A01]
+  char Codigo = "A01";
 //Cidade
-  char[São Paulo]
+  char Cidade = "São Paulo";
 //População
-  int 12325000
+  int População = 12325000;
 //Áarea em km²
-  float 1521.11
+  float Área_em_km² = 1521.11;
 //PIB
-  float 699.28 bilhões de reais
+  float PIB_em_Bilhões = 699.28;
 //N° Pontos turisticos
-  int 50
+  int PontosTuristicos = 50;
+  return 0;
+}
+int main() {
+  printf("carta 02!/n");
+//Estado
+  char Estado = "Rio de Janeiro";
+//Codigo
+  char Codigo = "A02";
+//Cidade
+  char Cidade = "Rio de Janeiro";
+//População
+  int População = 6748000;
+//Área em km²
+  float Área_em_km² = 1200.25;
+//PIB
+  float PIB_em_Bilhões= 300.50;
+//N° Pontos turisticos
+  int  PontosTuristicos = 30;
+  return 0;
 }
